e3c531c8be3b8be6a9a79e249f148ba815d16188
sheel <sheel@clik.ai>
2020-05-17 08:39:37 +0530

    first ssr

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import compression from "compression";
-
+import index from "./routes/index";
 import path from "path";
 
 // Server var
@@ -15,7 +15,8 @@ app.use(compression());
 console.log(__dirname);
 app.use(express.static(__dirname + "/public"));
 
-
+//Routes
+app.use("/", index);
 
 const port = process.env.PORT || 3000;
 

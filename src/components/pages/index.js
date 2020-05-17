33926db2d3319d39fa7ea411bc82c0172efedf82
sheel <sheel@clik.ai>
2020-05-17 09:04:58 +0530

    first hidrated ssr

--- src/components/pages/index.js
+++ src/components/pages/index.js
@@ -1,7 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Index() {
-    return <div>hello world!!</div>
+    const [text, setText] = useState('')
+
+
+    const onNameChangeHandler = (e) => {
+        setText(e.target.value)
+        console.log(e.target.value)
+    }
+
+    return (
+        <div>
+            <div>hello world!!</div>
+            <input name={"name-input"} onChange={onNameChangeHandler} type={"text"}
+                value={text} />
+        </div>
+    )
 }
 
 export default Index;
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+const config = {
+    entry: {
+        vendor: ["@babel/polyfill", "react"],
+        index: ["./src/components/entrypoint/index.jsx"]
+    },
+    output: {
+        path: path.resolve(__dirname, "src", "public"),
+        filename: "[name].js"
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env", "@babel/preset-react"]
+                    }
+                },
+                exclude: [/node_modules/, /public/]
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", ".jsx", ".json", ".wasm", ".mjs", "*"]
+    }
+};
+
+module.exports = config;
